f5a9dde307898a5d5d86072921e33abd4740dc54
Greg Pynes <gpynes@gmail.com>
2016-09-24 15:23:35 -0700

    Adds working tests

--- test.js
+++ test.js
@@ -1,15 +1,21 @@
 var express = require('express'),
     app     = express(),
     port    = 3000,
-    // server  = require('promised-sockets')(app);
-    io      = require('./socket.js');
+    ioProm  = require('./socket.js');
 
-    var server = io.init(app);
+    var server = ioProm.init(app);
 
-var nsp = io.of('/api');
+// Use a promise to set up the io part once the socket is created.
+ioProm.then(function(io) {
+    var nsp = io.of('/api');
 
-nsp.on('connection', function(socket) {
-    console.log('Connected!');
+    nsp.on('connection', function(socket) {
+        console.log('Connected!');
+        socket.on('test', function(data) {
+            console.log('Got data from test', data);
+            socket.emit('sendTest', data);
+        });
+    });
 });
 
 server.listen(port, function() {
